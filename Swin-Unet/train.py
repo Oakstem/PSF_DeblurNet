da6c92e7626dbae224323f118f2dda5a38d289ff
Alon <alonvbk@gmail.com>
2022-02-05 15:17:08 +0200

    disabled resume from default

--- Swin-Unet/train.py
+++ Swin-Unet/train.py
@@ -104,7 +104,8 @@ if __name__ == "__main__":
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
     net = ViT_seg(config, img_size=args.img_size, num_classes=args.num_classes).to(device)
-    net.load_from(config)
+    if args.resume:
+        net.load_from(config)
 
     trainer = {'Synapse': trainer_synapse, 'Monkaa': trainer_monkaa}
     trainer[dataset_name](args, net, args.output_dir, device)
