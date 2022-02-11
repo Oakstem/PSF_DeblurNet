350af5f0249b51db71285c8728b8454e6c6f172e
Alon <alonvbk@gmail.com>
2022-02-11 11:45:25 +0200

    dirty working stage of FlowNet

---
+++ FlowNetPytorch/datasets/loader/params.py
@@ -0,0 +1,12 @@
+from .type import Type
+from .sub_type import SubType
+
+
+class DataLoaderParams:
+    def __init__(self, type: Type, sub_types: [SubType], root_path: str,
+                 batch_size: int, shuffle: bool = False):
+        self.type: Type = type
+        self.sub_types: SubType = sub_types
+        self.root_path: str = root_path
+        self.batch_size: int = batch_size
+        self.shuffle: bool = shuffle
---
+++ FlowNetPytorch/datasets/loader/sub_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class SubType(Enum):
+    NOT_RELEVANT = "Not_Relevant"
+    PAST_LEFT = "Past_Left"
+    PAST_RIGHT = "Past_Right"
+    FUTURE_LEFT = "Future_Left"
+    FUTURE_RIGHT = "Future_Right"
