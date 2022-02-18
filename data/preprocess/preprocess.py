972044b44189df4a5e9dca80063dfad6688280d2
Michael <michael@mystate.cool>
2022-02-18 17:19:53 +0200

    Working on adding Flying Chairs

--- data/preprocess/preprocess.py
+++ data/preprocess/preprocess.py
@@ -90,6 +90,7 @@ def _apply_blur_monkaa(target_root: str, flow_root: str, images_list: [], total_
                     prev_processed_count = processed_count
                     t = time.time()
 
+
 def _apply_blur_flying_chairs(target_root: str, flow_root: str, images_list_left: [], images_list_right: [],
                               total_images_num: int, start_scene_index: int, resize: Resize, do_apply_gamma: bool,
                               side: str, device: int or str, cams: [camera_model], psfs: [Tensor]):
@@ -104,7 +105,7 @@ def _apply_blur_flying_chairs(target_root: str, flow_root: str, images_list_left
         blur_path, image_name = get_blurred_image_path(target_root, scene_name, img, side)
 
         # Get the OF
-        num_images, _ = get_interpolations_num_monkaa(flow_root, scene_name, image_name, side)
+        num_images, _ = get_interpolations_num_flying_chairs(flow_root, blur_path, image_name)
 
         if num_images is not None:
             if device == torch.device("cuda"):
@@ -137,6 +138,15 @@ def get_interpolations_num_monkaa(flow_root: str, scene_name: str, img_name: str
     # Find L2 distance of flow movement, if within threshold, return number of frame interpolation
     pfm_file_name = f"into_future/{side}/OpticalFlowIntoFuture_{img_name[:-4]}_{letter}.pfm"
     optical_flow_file_path = os.path.join(flow_root, scene_name, pfm_file_name)
+
+    return _get_interpolations_num(optical_flow_file_path, min_nb, max_pxl_step, scale_reduct)
+
+
+def get_interpolations_num_flying_chairs(flow_root: str, folder_name: str, img_name: str, min_nb: int = 5,
+                                         max_pxl_step: int = 170, scale_reduct: int = 2):
+    # Find L2 distance of flow movement, if within threshold, return number of frame interpolation
+    pfm_file_name = f"{img_name[:-4]}.pfm"
+    optical_flow_file_path = os.path.join(flow_root, folder_name, pfm_file_name)
 
     return _get_interpolations_num(optical_flow_file_path, min_nb, max_pxl_step, scale_reduct)
 
